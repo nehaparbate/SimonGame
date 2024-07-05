4ca101edd921a7f88c178bff17e77ace99cc80ca
nehaparbate <parbate.neha20@gmail.com>
2024-07-04 22:20:54 -0500

    Add javascript code to generate a random number and color

---
+++ game.js
@@ -0,0 +1,9 @@
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+
+}
+gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomNumber);
