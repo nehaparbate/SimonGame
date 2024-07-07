eba2ebc91fa82eb6a75ae95e89740d659c467865
nehaparbate <parbate.neha20@gmail.com>
2024-07-06 22:05:45 -0500

    Add flashing of the squared and sound

--- game.js
+++ game.js
@@ -2,8 +2,23 @@ function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
 
 }
+var randomNumber = Math.floor(Math.random() * 4);
 gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var randomChosenColour = buttonColours[randomNumber];
 gamePattern.push(randomNumber);
+
+
+
+
+var element = $("#" + randomChosenColour);
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+
+var audioString = "sounds\\"+randomChosenColour + ".mp3";
+console.log(audioString);
+var audio = new Audio(audioString);
+audio.play();
+
+
+console.log(element)
