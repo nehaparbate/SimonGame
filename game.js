45534b0726e70032423e713cd90c207c069be2fc
nehaparbate <parbate.neha20@gmail.com>
2024-07-07 22:48:46 -0500

    Add event listeners for each color click by the user

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
-gamePattern = [];
+var gamePattern = [];
 var randomNumber;
 var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern =[];
 
 function nextSequence(){
     randomNumber = Math.floor(Math.random() * 4);
@@ -9,19 +10,47 @@ function nextSequence(){
 
 }
 
-function playAudio(){
+function playAudio(randomChosenColour){
     var audioString = "sounds\\"+randomChosenColour + ".mp3";
     console.log(audioString);
     var audio = new Audio(audioString);
     audio.play();
 }
 
-function showAnimation()
+function showAnimation(randomChosenColour)
 {
 var element = $("#" + randomChosenColour);
 $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-
 }
 
+
+$( "#yellow" ).on( "click", function(event) {
+    console.log(event.target.id);
+    userClickedPattern.push(event.target.id);
+    showAnimation(event.target.id);
+    playAudio(event.target.id);
+
+  } );
+  $( "#green" ).on( "click", function(event) {
+    console.log(event.target.id);
+    userClickedPattern.push(event.target.id);
+    showAnimation(event.target.id);
+    playAudio(event.target.id);
+
+  } );$( "#red" ).on( "click", function(event) {
+    console.log(event.target.id);
+    userClickedPattern.push(event.target.id);
+    showAnimation(event.target.id);
+    playAudio(event.target.id);
+
+  } );$( "#blue" ).on( "click", function(event) {
+    console.log(event.target.id);
+    userClickedPattern.push(event.target.id);
+    showAnimation(event.target.id);
+    playAudio(event.target.id);
+
+  } );
+
+  
 
 
