62eef9532d908e70fb111c9a03d392b9c3689e66
nehaparbate <parbate.neha20@gmail.com>
2024-07-07 19:53:36 -0500

    Refactor code

--- game.js
+++ game.js
@@ -1,24 +1,27 @@
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random() * 4);
-
-}
-var randomNumber = Math.floor(Math.random() * 4);
 gamePattern = [];
+var randomNumber;
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomNumber);
+function nextSequence(){
+    randomNumber = Math.floor(Math.random() * 4);
+    randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomNumber);
 
+}
 
+function playAudio(){
+    var audioString = "sounds\\"+randomChosenColour + ".mp3";
+    console.log(audioString);
+    var audio = new Audio(audioString);
+    audio.play();
+}
 
-
+function showAnimation()
+{
 var element = $("#" + randomChosenColour);
 $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
 
-var audioString = "sounds\\"+randomChosenColour + ".mp3";
-console.log(audioString);
-var audio = new Audio(audioString);
-audio.play();
+}
 
 
-console.log(element)
+
