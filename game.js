450f4ec385f15d576c99de2ef92624920e58a7b0
nehaparbate <parbate.neha20@gmail.com>
2024-07-12 19:54:17 -0500

    Add variables for patterns

--- game.js
+++ game.js
@@ -2,15 +2,24 @@ var gamePattern = [];
 var randomNumber;
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern =[];
+var level = 0;
+var isGameStarted = 0;
 
 function nextSequence(){
+
+    isGameStarted = 1;
+    $("h1").text("Level "+ level);
     randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomNumber);
+    showAnimation(randomChosenColour);
+    level = level + 1;
 
 }
 
-function playAudio(randomChosenColour){
+function playAudio(randomChosenColour)
+{
+  
     var audioString = "sounds\\"+randomChosenColour + ".mp3";
     console.log(audioString);
     var audio = new Audio(audioString);
@@ -23,6 +32,15 @@ var element = $("#" + randomChosenColour);
 $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
 }
 
+function handleKeyPress(event) {
+  if (isGameStarted == 0){
+    nextSequence();
+  }
+
+}
+
+// Add the event listener to the document
+document.addEventListener('keypress', handleKeyPress);
 
 $( "#yellow" ).on( "click", function(event) {
     console.log(event.target.id);
