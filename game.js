7446241cd09aaf7a3f0f8e9606ab8e5f7ef5452d
nehaparbate <parbate.neha20@gmail.com>
2024-07-15 21:38:25 -0500

    Add function to check answer

--- game.js
+++ game.js
@@ -33,39 +33,82 @@ $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
 }
 
 function handleKeyPress(event) {
+
   if (isGameStarted == 0){
-    nextSequence();
+    // Add delay of 3 second (3000 milliseconds) before calling nextSequence
+    setTimeout(function() {
+      nextSequence();
+    }, 2000);
   }
 
 }
 
+function checkArray(currentLevel)
+{
+  console.log(gamePattern);
+  console.log(userClickedPattern);
+  if (userClickedPattern.length !== gamePattern.length) {
+    return false;
+}
+for (var i = 0; i < userClickedPattern.length; i++) {
+    if (gamePattern[i] !== userClickedPattern[i]) {
+        return false;
+    }
+}
+return true;
+
+}
+
+function checkAnswer(){
+
+      if (checkArray() == false)
+      {
+alert("Game Over");
+      }
+      else{
+ // Add delay of 3 second (3000 milliseconds) before calling nextSequence
+ setTimeout(function() {
+  nextSequence();
+}, 3000);
+      }
+
+
+
+}
 // Add the event listener to the document
 document.addEventListener('keypress', handleKeyPress);
 
-$( "#yellow" ).on( "click", function(event) {
+$("#yellow").on( "click", function(event) {
+
     console.log(event.target.id);
-    userClickedPattern.push(event.target.id);
+    userClickedPattern.push(  buttonColours.indexOf( event.target.id) );
     showAnimation(event.target.id);
     playAudio(event.target.id);
+    checkAnswer();
 
   } );
+
   $( "#green" ).on( "click", function(event) {
     console.log(event.target.id);
-    userClickedPattern.push(event.target.id);
+    userClickedPattern.push(buttonColours.indexOf( event.target.id));
     showAnimation(event.target.id);
     playAudio(event.target.id);
-
+    checkAnswer();
+ 
   } );$( "#red" ).on( "click", function(event) {
     console.log(event.target.id);
-    userClickedPattern.push(event.target.id);
+    userClickedPattern.push(buttonColours.indexOf( event.target.id));
     showAnimation(event.target.id);
     playAudio(event.target.id);
+    checkAnswer();
+
 
   } );$( "#blue" ).on( "click", function(event) {
     console.log(event.target.id);
-    userClickedPattern.push(event.target.id);
+    userClickedPattern.push(buttonColours.indexOf( event.target.id));
     showAnimation(event.target.id);
     playAudio(event.target.id);
+    checkAnswer();
 
   } );
 
